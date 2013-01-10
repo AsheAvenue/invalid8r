96f69e3bee48af278967262332dce3e553dad8da
Tim Boisvert <tboisvert@asheavenue.com>
2013-01-10 17:21:17 -0500

    pull out config

--- app/controllers/invalid8_controller.rb
+++ app/controllers/invalid8_controller.rb
@@ -1,10 +1,15 @@
 class Invalid8Controller < ApplicationController
   
-  def index  
+  def index
   end
   
   def handle
+    #get the site key
     @sitekey = params[:sitekey]
+    
+    #find the config element that matches the sitekey
+    @test = Application.config
+    
     @path = request.query_string
   end
   
